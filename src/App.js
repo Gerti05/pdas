45eb724fb6b4a8aa8913348f3fb342b2cd4aa3fd
Gerti <GertiGoga@gmail.com>
2023-01-29 18:17:23 -0800

    Added images

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import ProfileCard from "./ProfileCard";
+import AlexaImg from "./images/alexa.png";
+import CortanaImg from "./images/cortana.png";
+import SiriImg from "./images/siri.png";
 
 function App() {
   return (
     <div>
       <div>Personal Digital Assistance</div>
-      <ProfileCard />
-      <ProfileCard />
-      <ProfileCard />
+
+      <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImg} />
+      <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImg} />
+      <ProfileCard title="Siri" handle="@siri01" image={SiriImg} />
     </div>
   );
 }
